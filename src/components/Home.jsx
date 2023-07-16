7db81c47a51029c52fae786e9a8dec8b5512772b
Giorgi <129410101+giorgisar@users.noreply.github.com>
2023-07-16 21:27:15 +0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -17,12 +18,14 @@ const Home = () => {
         <h2 className="text-4xl sm:text-7xl font-bold leading-snug relative text-transparent bg-clip-text bg-gradient-to-r from-slate-600 via-purple-900 to-slate-800">
           I'm a Front-End Web Developer
         </h2>
-        <button className="w-[180px] text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 ">
-          View Work
-          <span className="group-hover:rotate-90 duration-300">
-            <HiArrowNarrowRight className="ml-3" />
-          </span>
-        </button>
+        <Link to="work" smooth={true} duration={500}>
+          <button className="w-[180px] text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 hover:scale-105 duration-500">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </Link>
       </div>
     </div>
   );
