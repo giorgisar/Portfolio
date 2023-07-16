1d9d4cfd2c4cd54c9d195d2b7618d15d610ccad1
Giorgi <129410101+giorgisar@users.noreply.github.com>
2023-07-16 21:28:05 +0400

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import workImg from "../assets/workImg.jpeg";
-import realEstate from "../assets/realestate.jpg";
+import bankist from "../assets/bankist.png";
+import finance from "../assets/finance.png";
+import weekaway from "../assets/weekaway.png";
+import pig from "../assets/pig.png";
+import bankist1 from "../assets/bankist1.png";
 
 const Work = () => {
   return (
@@ -17,21 +20,29 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${workImg})` }}
+            style={{ backgroundImage: `url(${bankist})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover */}
             <div className="opacity-0 group-hover:opacity-100 ">
               <span className=" text-2xl font-bold text-white tracking-wider">
-                React Js Application
+                Javascript Application
               </span>
               <div className="pt-8 text-center">
-                <a href="">
+                <a
+                  href="https://64b41ce52a1b904e20d9d0ea--sage-truffle-c51f9b.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a
+                  href="https://github.com/giorgisar/Bankist"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
                     {" "}
                     Code
@@ -41,7 +52,7 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${realEstate})` }}
+            style={{ backgroundImage: `url(${finance})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover */}
@@ -50,12 +61,20 @@ const Work = () => {
                 React Js Application
               </span>
               <div className="pt-8 text-center">
-                <a href="">
+                <a
+                  href="https://giorgisar.github.io/Finance/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a
+                  href="https://github.com/giorgisar/Finance"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
                     {" "}
                     Code
@@ -65,7 +84,7 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${workImg})` }}
+            style={{ backgroundImage: `url(${weekaway})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover */}
@@ -74,12 +93,20 @@ const Work = () => {
                 React Js Application
               </span>
               <div className="pt-8 text-center">
-                <a href="">
+                <a
+                  href="https://giorgisar.github.io/Companion/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a
+                  href="https://github.com/giorgisar/Companion"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
                     {" "}
                     Code
@@ -89,21 +116,29 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${realEstate})` }}
+            style={{ backgroundImage: `url(${pig})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className=" text-2xl font-bold text-white tracking-wider">
-                React Js Application
+                Javascript Application
               </span>
               <div className="pt-8 text-center">
-                <a href="">
+                <a
+                  href="https://iridescent-gelato-7d50ed.netlify.app/ "
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a
+                  href="https://github.com/giorgisar/Pig"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
                     {" "}
                     Code
@@ -113,47 +148,30 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${workImg})` }}
+            style={{ backgroundImage: `url(${bankist1})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className=" text-2xl font-bold text-white tracking-wider">
-                React Js Application
+                Javascript Application
               </span>
               <div className="pt-8 text-center">
-                <a href="">
+                <a
+                  href="https://starlit-cobbler-0a6e36.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
                     Demo
                   </button>
                 </a>
-                <a href="">
+                <a
+                  href="https://github.com/giorgisar/Bankist1"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
-                    {" "}
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className=" text-2xl font-bold text-white tracking-wider">
-                React Js Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
-                    Demo
-                  </button>
-                </a>
-                <a href="">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg hover:scale-110 duration-500">
-                    {" "}
                     Code
                   </button>
                 </a>
